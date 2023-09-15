34ea54da5ff9082456c1e6c7b82c978f5cd1ba7f
Jacob Ebey <jacob.ebey@live.com>
2023-09-15 01:06:53 -0700

    organize file

--- framework/src/ssr/entry.ts
+++ framework/src/ssr/entry.ts
@@ -4,65 +4,6 @@ import * as ReactDOM from "#react-dom-server-implementation";
 // @ts-ignore
 import * as ReactDOMClient from "#react-server-dom-client-implementation";
 
-async function RecursiveRSCRenderer({
-	decoder,
-	reader,
-}: {
-	decoder: TextDecoder;
-	reader: ReadableStreamDefaultReader<Uint8Array>;
-}): Promise<React.ReactElement> {
-	const result = await reader.read();
-	const chunk = result.value;
-	const chunkString = decoder.decode(chunk, { stream: true });
-	await new Promise((resolve) => setTimeout(resolve, 0));
-	const script = React.createElement("script", {
-		// biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
-		dangerouslySetInnerHTML: {
-			// TODO: encode the chunk
-			__html: ` __RSC_CHUNK__(${sanitize(JSON.stringify(chunkString))});`,
-		},
-	});
-	if (result.done) {
-		return React.createElement(
-			React.Fragment,
-			{},
-			script,
-			React.createElement("script", {
-				// biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
-				dangerouslySetInnerHTML: {
-					__html: "__RSC_CONTROLLER__.close();",
-				},
-			}),
-		);
-	}
-	return React.createElement(
-		React.Fragment,
-		{},
-		script,
-		React.createElement(
-			React.Suspense,
-			{},
-			React.createElement(RecursiveRSCRenderer, { decoder, reader }),
-		),
-	);
-}
-
-// Taken from https://github.com/cyco130/vite-rsc/blob/2e3d0ad9915e57c4b2eaa3ea24b46c1b477a4cce/packages/fully-react/src/server/htmlescape.ts#L25C1-L38C2
-const TERMINATORS_LOOKUP: Record<string, string> = {
-	"\u2028": "\\u2028",
-	"\u2029": "\\u2029",
-};
-
-const TERMINATORS_REGEX = /[\u2028\u2029]/g;
-
-function sanitizer(match: string | number) {
-	return TERMINATORS_LOOKUP[match];
-}
-
-export function sanitize(str: string) {
-	return str.replace(TERMINATORS_REGEX, sanitizer);
-}
-
 export async function fetch(
 	request: Request,
 	{
@@ -121,4 +62,63 @@ export async function fetch(
 			"Transfer-Encoding": "chunked",
 		},
 	});
+}
+
+async function RecursiveRSCRenderer({
+	decoder,
+	reader,
+}: {
+	decoder: TextDecoder;
+	reader: ReadableStreamDefaultReader<Uint8Array>;
+}): Promise<React.ReactElement> {
+	const result = await reader.read();
+	const chunk = result.value;
+	const chunkString = decoder.decode(chunk, { stream: true });
+	await new Promise((resolve) => setTimeout(resolve, 0));
+	const script = React.createElement("script", {
+		// biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
+		dangerouslySetInnerHTML: {
+			// TODO: encode the chunk
+			__html: ` __RSC_CHUNK__(${sanitize(JSON.stringify(chunkString))});`,
+		},
+	});
+	if (result.done) {
+		return React.createElement(
+			React.Fragment,
+			{},
+			script,
+			React.createElement("script", {
+				// biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
+				dangerouslySetInnerHTML: {
+					__html: "__RSC_CONTROLLER__.close();",
+				},
+			}),
+		);
+	}
+	return React.createElement(
+		React.Fragment,
+		{},
+		script,
+		React.createElement(
+			React.Suspense,
+			{},
+			React.createElement(RecursiveRSCRenderer, { decoder, reader }),
+		),
+	);
+}
+
+// Taken from https://github.com/cyco130/vite-rsc/blob/2e3d0ad9915e57c4b2eaa3ea24b46c1b477a4cce/packages/fully-react/src/server/htmlescape.ts#L25C1-L38C2
+const TERMINATORS_LOOKUP: Record<string, string> = {
+	"\u2028": "\\u2028",
+	"\u2029": "\\u2029",
+};
+
+const TERMINATORS_REGEX = /[\u2028\u2029]/g;
+
+function sanitizer(match: string | number) {
+	return TERMINATORS_LOOKUP[match];
+}
+
+export function sanitize(str: string) {
+	return str.replace(TERMINATORS_REGEX, sanitizer);
 }
